22aa779198adb30a49654224caabbf7a93af1c3a
Jacob Arriola <jacob.arriola@gmail.com>
2017-06-08 07:33:38 -0700

    Reformat to 2 spaces - formatting

--- src/components/Todo-Item.vue
+++ src/components/Todo-Item.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="container">
-        <input type="checkbox" v-model="todo.completed">
-        <div>
-            <h2>{{ todo.title }}</h2>
-            <p>{{ todo.notes }}</p>
-        </div>
+  <div class="container">
+    <input type="checkbox" v-model="todo.completed">
+    <div>
+      <h2>{{ todo.title }}</h2>
+      <p>{{ todo.notes }}</p>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'todoItem',
-        props: {
-            todo: {
-                type: Object,
-                required: true
-            }
-        }
+export default {
+  name: 'todoItem',
+    props: {
+    todo: {
+      type: Object,
+      required: true
     }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    h2 {
-        font-size: 1.5em;
-        line-height: 1;
-        margin-bottom: .25em;
-        margin-top: 0;
-    }
+  h2 {
+    font-size: 1.5em;
+    line-height: 1;
+    margin-bottom: .25em;
+    margin-top: 0;
+  }
 
-    p {
-        margin-top: 0;
-    }
+  p {
+    margin-top: 0;
+  }
 
-    .container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-    input[type="checkbox"] {
-        font-size: 3em;
-    }
+  input[type="checkbox"] {
+    font-size: 3em;
+  }
 </style>
--- src/components/Todo-List.vue
+++ src/components/Todo-List.vue
@@ -1,35 +1,35 @@
 <template>
-    <div>
-        <p>I'm the todos component</p>
-        <ul>
-            <li v-for="todo in todos">
-                <todo-item :todo="todo"></todo-item>            
-            </li>
-        </ul>
-    </div>
+  <div>
+    <p>I'm the todos component</p>
+    <ul>
+      <li v-for="todo in todos">
+        <todo-item :todo="todo"></todo-item>            
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    import todoItem from './Todo-Item.vue';
-    import { data } from '../sample-data.js';
+import todoItem from './Todo-Item.vue';
+import { data } from '../sample-data.js';
 
-    export default {
-        name: 'todoList',
-        components: { todoItem },
-        data() {
-            return {
-                todos: data
-            }
-        }
+export default {
+  name: 'todoList',
+  components: { todoItem },
+  data() {
+    return {
+      todos: data
     }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    ul {
-        margin-top: 2.5em;
-    }
+ul {
+  margin-top: 2.5em;
+}
 
-    li {
-        margin-bottom: .75em;
-    }
+li {
+  margin-bottom: .75em;
+}
 </style>
