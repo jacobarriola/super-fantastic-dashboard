f8150b8a74845f41ca7bf6749f714da2660f8b0f
Stephen Fox <sfoxorama@gmail.com>
2017-06-15 21:39:18 -0400

    Clean up background_image_loader.js. Consistent use of single quotes in js. Removed comments and console.log() statements.

--- static/js/background_image_loader.js
+++ static/js/background_image_loader.js
@@ -1,50 +1,25 @@
 (function () {
-  "use strict";
+  'use strict';
 
-  // Define palette of fallback colors
-  const COLORS = [
-    '#1abc9c', // Turquoise
-    '#2ecc71', // Emerald
-    '#3498db', // Peter River
-    '#9b59b6', // Amethyst
-    '#34495e', // Wet Asphalt
-    '#16a085', // Green Sea
-    '#27ae60', // Nephritis
-    '#2980b9', // Belize Hole
-    '#8e44ad', // Wisteria
-    '#2c3e50', // Midnight Blue
-    '#f1c40f', // Sun Flower
-    '#e67e22', // Carrot
-    '#e74c3c', // Alizarin
-    '#95a5a6', // Concrete
-    '#f39c12', // Orange
-    '#d35400', // Pumpkin
-    '#c0392b', // Pomegranate
-    '#7f8c8d' // Asbestos
-  ];
+  const colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#16a085', '#27ae60',
+    '#2980b9', '#8e44ad', '#2c3e50', '#f1c40f', '#e67e22', '#e74c3c', '#95a5a6', '#f39c12',
+    '#d35400', '#c0392b', '#7f8c8d'];
 
-  // Randomly select fallback color
-  const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+  const color = colors[Math.floor(Math.random() * colors.length)];
 
-  // Build request
   const request = new Request('https://api.unsplash.com/photos/random?featured&orientation=landscape&collections=135648', {
     method: 'GET',
     headers: new Headers({
-      Authorization: 'Client-ID b325d6aa7cdaacc3b279aa500e0f091c1e3ae2b986270547a005dfe1835b78a9'
-    })
+      Authorization: 'Client-ID b325d6aa7cdaacc3b279aa500e0f091c1e3ae2b986270547a005dfe1835b78a9',
+    }),
   });
 
-  // Make the request with fetch
   fetch(request)
     .then(response => response.json())
     .then((response) => {
-      // console.log(response);
       document.body.style.backgroundImage = `url(${response.urls.regular})`;
     })
-    .catch((error) => {
-      console.log(error);
-      // On error, set the background color to randomly selected color
+    .catch(() => {
       document.body.style.backgroundColor = color;
     });
-
 }());
